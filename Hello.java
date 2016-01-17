4371c63434d61271d00f81bad0e7e4f8ffbc78a1
guol <56097524@qq.com>
2016-01-17 23:52:39 +0800

    update brh Hello.java file

--- Hello.java
+++ Hello.java
@@ -5,5 +5,7 @@ public class Hello
 		System.out.println("hello !!");
 		System.out.println("http://gltt1314.kmdns.net/ !!");
 		System.out.println("http://www.llaitt1314.com/ !!");
+		System.out.println("brh branch update!!!");
+		System.out.println("hello brh branch update!!!");
 	}
 }
