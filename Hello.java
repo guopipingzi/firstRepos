c8c98ca5123883c62f3c1b1d0cbb318f1c953e79
guol <56097524@qq.com>
2016-01-18 00:07:59 +0800

    rm lines on master

--- Hello.java
+++ Hello.java
@@ -2,7 +2,6 @@ public class Hello
 {
 	public static void main(String[] agrs)
 	{
-		System.out.println("hello !!");
 		System.out.println("http://gltt1314.kmdns.net/ !!");
 		System.out.println("http://www.llaitt1314.com/ !!");
 	}
