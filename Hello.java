281925661965c4e881799156e220d2d9cb0a76c9
guol <56097524@qq.com>
2016-01-17 16:09:04 +0800

    update Hello.java file . add two lines

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello
 {
 	public static void main(String[] agrs)
 	{
-		System.out.println("hello git!!");
+		System.out.println("hello !!");
+		System.out.println("http://gltt1314.kmdns.net/ !!");
 	}
 }
