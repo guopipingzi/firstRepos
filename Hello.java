1041a5c0ec8d80846a0eb405a78dfbe7f531acd4
guol <56097524@qq.com>
2016-01-18 12:08:17 +0800

    hello print

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello
 {
 	public static void main(String[] agrs)
 	{
-		System.out.println("*******");
-		System.out.println("#######");
+		System.out.println("Hello World");
+		System.out.println("Hello guol!!!");
 	}
 }
